3b0c2d6962cb5a939464624599016b9562b96f3a
VanessaDaSilva <vanessa.silva@amo.delivery>
2023-11-10 17:41:11 -0300

    add error handling

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require ("../controladores/livro")
+const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require("../controladores/livro")
 
 const router = Router()
 
--- serviços/livro.js
+++ serviços/livro.js
@@ -1,13 +1,13 @@
 const fs = require("fs")
 
 function getTodosLivros() {
-    return JSON.parse( fs.readFileSync("livros.json") )
+    return JSON.parse(fs.readFileSync("livros.json"))
 }
 
 function getLivroPorId(id) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const livroFiltrado = livros.filter( livro => livro.id === id) [0]
+    const livroFiltrado = livros.filter(livro => livro.id === id)[0]
     return livroFiltrado
 }
 
@@ -21,9 +21,9 @@ function insereLivro(livroNovo) {
 
 function modificaLivro(modificacoes, id) {
     let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
-    const indiceModificado = livrosAtuais.findIndex(livro => livro.id ===id)
+    const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
 
-    const conteudoMudado = {...livrosAtuais[indiceModificado], ...modificacoes}
+    const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
     //livrosAtuais[indiceModificado] -> {id:"2", nome "livro irado"}
     //modificacoes -> {nome: "nome mucho legal"}
 
@@ -35,7 +35,7 @@ function modificaLivro(modificacoes, id) {
 function deletaLivroPorId(id) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const livrosFiltrados = livros.filter( livro => livro.id !== id )
+    const livrosFiltrados = livros.filter(livro => livro.id !== id)
     fs.writeFileSync("livros.json", JSON.stringify(livrosFiltrados))
 }
 
@@ -43,6 +43,6 @@ module.exports = {
     getTodosLivros,
     getLivroPorId,
     insereLivro,
-    modificaLivro, 
+    modificaLivro,
     deletaLivroPorId
 }
